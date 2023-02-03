987f9e598e1970b1457fe2dcabad56beac515102
unknown <Savioanjos2020@gmail.com>
2023-02-02 23:47:24 -0300

    Criando tabela de usuarios no banco de dados

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,9 @@
 import {Router, Request, Response} from 'express';
 
+
  const router = Router();
 
- router.get('/teste', (req: Request, res: Response) => {
-     return res.json({ok: true})
- })
+ //-- ROTAS USER --
+
 
  export { router };
