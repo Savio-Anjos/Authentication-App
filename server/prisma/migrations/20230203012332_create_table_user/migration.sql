1d05a12dca4f65fdfb285a32556dfa916d60973f
unknown <Savioanjos2020@gmail.com>
2023-02-02 22:27:52 -0300

    Criando tabela de usuarios no banco de dados

---
+++ server/prisma/migrations/20230203012332_create_table_user/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "bio" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL
+);
